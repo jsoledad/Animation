3f0d54557b302275133292aed4da99be1e8622c0
jonathan <jonathansoledadmartinez@gmail.com>
2021-01-17 01:09:55 -0800

    more testing with padding

--- chief.js
+++ chief.js
@@ -6,7 +6,7 @@ class Chief{
         this.background = ASSET_MANAGER.getAsset("./sprites/background.png");
 
         this.facing = 0; // 0 = right, 1 = left, 2 = down, 3 = up
-        this.steps = 0; // number of steps skeleton has taken
+     
         this.animations = [];
         this.loadAnimations();
         //this.animation = new Animator(this.spritesheet, 80, 205, 30, 50, 8, 0.15, 34, false, true);
@@ -20,7 +20,7 @@ class Chief{
         }
        // constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop)
         // walking right
-        this.animations[0] = new Animator(this.spritesheet, 117, 9, 41, 60, 11, 0.17, 10, false, true);
+        this.animations[0] = new Animator(this.spritesheet, 117, 9, 41, 60, 10, 0.17, 10, false, true);
 
         // Just in case more directions, currently only right is available
         this.animations[1] = new Animator(this.spritesheet, 0, 0, 0, 0, 0, 0, 0, false, true);
@@ -36,10 +36,6 @@ class Chief{
     draw(ctx) {
         ctx.drawImage(this.background, 0, 0, 288 * 3.6, 160 * 3.6);
 
-        this.animations[this.facing].drawFrame(this.game.clockTick, ctx, -100  + this.steps * 2, 300, 4);
-        this.steps++;
-        if (this.steps === 550) {
-            this.steps = 0;
-        }
+        
     }
 }
